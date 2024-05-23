8bb0d4c4cbb218d577322a746098d9eae624e1bd
Camilo <camilomedina.es@gmail.com>
2024-05-22 21:28:46 -0400

    alert al algoritmo

--- main.js
+++ main.js
@@ -2,39 +2,47 @@
 
 const calculadoraDeComprasProveedores = () => {
 
-const reciboDePago = [];
+    const reciboDePago = [];
 
-while (true) {
-    const producto = prompt('Ingresa un producto (o escribe "fin" para terminar):');
-    if (producto.toLowerCase() === 'fin') {
-    break;
-    }
-    const precio = parseFloat(prompt(`Ingresa el precio NETO unitario del ${producto} (en pesos chilenos):`));
-    const cantidad = Number(prompt(`Ingresa cantidades compradas por Kg o Un de ${producto} (si compras menos de 1 kg ingresa el valor en decimales con un punto ej: 0.5)`));
+        while (true) {
+        const producto = prompt('Ingresa un producto (o escribe "fin" para terminar):');
 
-    const costoTotal = precio * cantidad;
-    reciboDePago.push({ producto, precio, cantidad, costoTotal });
-}
+        if (producto.toLowerCase() === 'fin') {
+            break;
+        };
+        const precio = parseFloat(prompt(`Ingresa el precio NETO unitario del ${producto} (en pesos chilenos):`));
+        const cantidad = Number(prompt(`Ingresa cantidades compradas por Kg o Un de ${producto} (si compras menos de 1 kg ingresa el valor en decimales con un punto ej: 0.5)`));
 
-console.log('Detalle proveedor:');
-for (const item of reciboDePago) {
-console.log(`${item.producto} - Precio: ${item.precio} CLP - Cantidad: ${item.cantidad} - Costo total: ${item.costoTotal} CLP`);
-}
+        const costoTotal = precio * cantidad;
+        reciboDePago.push({ producto, precio, cantidad, costoTotal });
+    };
 
-let suma = 0;
-for (let i = 0; i < reciboDePago.length; i++) {
+    let suma = 0;
+    for (let i = 0; i < reciboDePago.length; i++) {
     suma += reciboDePago[i].costoTotal;
-}
-console.log('Total neto: '+ suma);
+    };
 
-let totalMasIva = 0;
-totalMasIva = suma * 1.19;
-console.log('Total bruto: '+ totalMasIva);
+    console.log('Detalle proveedor:');
+    for (const item of reciboDePago) {
+        console.log(`${item.producto} - Precio: ${item.precio} CLP - Cantidad: ${item.cantidad} - Costo total: ${item.costoTotal} CLP`);
+    };
+    console.log('Total neto: '+ suma + ' CLP');
 
-let totalImpuestoAPagar = 0;
-totalImpuestoAPagar = suma * 0.19;
-console.log('Impuesto por pagar: '+ totalImpuestoAPagar)
+    let totalMasIva = 0;
+    totalMasIva = suma * 1.19;
+    console.log('Total bruto (IVA 19%): '+ totalMasIva + ' CLP');
 
-}
+    let totalImpuestoAPagar = 0;
+    totalImpuestoAPagar = suma * 0.19;
+    console.log('Impuesto por pagar: '+ totalImpuestoAPagar + ' CLP');
+
+    for (const item of reciboDePago) {
+        alert(`${item.producto} - Precio: ${item.precio} CLP - Cantidad: ${item.cantidad} - Costo total: ${item.costoTotal} CLP`);
+    };
+
+    alert('Total neto: ' + suma + ' CLP ' + '-' +' Total bruto (IVA 19%) : '+ totalMasIva + ' CLP ' + '-' +' Impuesto por pagar: '+ totalImpuestoAPagar + ' CLP ');
+
+};
 
 calculadoraDeComprasProveedores();
+
